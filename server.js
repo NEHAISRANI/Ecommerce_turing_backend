9a80f6d2a32e22c2cf3835567d10beb832d07704
NEHAISRANI <nehai18@navgurukul.org>
2020-10-09 22:41:36 +0530

    added server file require express and body parser.Body paarser is using for parsing the incoming request in a middleware .port number also have given.by server file only our routes will run

---
+++ server.js
@@ -0,0 +1,29 @@
+const express = require("express");
+const app = express();
+
+var bodyParser = require("body-parser");
+app.use(bodyParser.json()); 
+
+const department=require("./router/department")
+app.use(department) 
+
+const categories=require("./router/categories")
+app.use(categories) 
+
+const attributes=require("./router/attribute")
+app.use(attributes)
+
+const product=require("./router/product")
+app.use(product)
+
+const customer=require("./router/customer")
+app.use(customer)
+
+const orders=require("./router/orders")
+app.use(orders)
+
+const server = app.listen(3600, function(){     
+  console.log("listening on port %s...", server.address().port);
+});  
+
+
